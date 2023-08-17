083e64d0103746dbc32f3e47b47d31c27faa9668
Igor Saiti <saitiigor@gmail.com>
2023-08-17 11:16:17 -0300

    Finalização dos testes de usuário

--- src/usecases/tests/users.controller.spec.ts
+++ src/usecases/tests/users.controller.spec.ts
@@ -1,4 +1,3 @@
-import { DBConnection } from "../../database";
 import { User } from "../../models/user.model";
 import { CreateUserUseCaseExecute } from "../create-user.usecase";
 
@@ -15,15 +14,26 @@ const users = {
     create
 }
 
-const mockDBConnection = ({
+const mockDBConnection = {
     users, 
-} as unknown) as DBConnection; // também pode ser "as any" ao invés de "unknown) as DBConnection"
+} as any // também pode ser "as any" ao invés de "unknown as DBConnection"
 
 describe("UsersController", () => {
     it("Deve ser capaz de criar um usuário", async() => {        
         const user = await CreateUserUseCaseExecute('igor', 'senha123', mockDBConnection)
         
         expect((user as User).username).toBe('igor')
-    })
+    });
 
+    it("should not be able to create a user if there is already a user with the same username", async() => {
+        mockDBConnection.users.findOne = (username: string) => {
+            return {
+                username,
+            }
+        }
+
+        const res = await CreateUserUseCaseExecute('igor', 'senha123', mockDBConnection)
+        
+        expect(res['statusCode']).toBe(409)
+    });
 })
